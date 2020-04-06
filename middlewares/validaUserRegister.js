9e54453778fa3cbe4fb925b37c5835464ae6bf64
brunolimadev <bruno.lima.dev@outlook.com>
2020-04-06 14:07:13 -0300

    Criada verificação de usuário existente no cadastro de usuarios

--- middlewares/validaUserRegister.js
+++ middlewares/validaUserRegister.js
@@ -1,30 +1,44 @@
-const {check, validationResult, body } = require('express-validator')
+const {
+    check,
+    validationResult,
+    body
+} = require('express-validator')
 const UserController = require('../controllers/UserController')
 const bcrypt = require('bcrypt')
 const fs = require('fs')
 const path = require('path')
 
-const userRegister = (req, res, next) =>{
+const userRegister = (req, res, next) => {
     const erro = validationResult(req)
-    if(erro.isEmpty()){
+    if (erro.isEmpty()) {
 
-
-        if(!fs.existsSync(path.join('db','users.json'))){
-            fs.writeFileSync(path.join('db','users.json'),JSON.stringify([]))
+        if (!fs.existsSync(path.join('db', 'users.json'))) {
+            fs.writeFileSync(path.join('db', 'users.json'), JSON.stringify([]))
         }
-
-        let fileUsers = JSON.parse(fs.readFileSync(path.join('db','users.json'),{encoding: 'utf-8'})) 
+        let fileUsers = JSON.parse(fs.readFileSync(path.join('db', 'users.json'), {
+            encoding: 'utf-8'
+        }))
         let email = req.body.email
 
-        if(fileUsers.filter(user => { return user.email == email }).length > 0){
-            res.render('register', {css: UserController.pageData.css, erros: [{msg: 'O usuário já possui cadastro!'}]})
-        }else{
+        if (fileUsers.filter(user => {
+                return user.email == email
+            }).length > 0) {
+            res.render('register', {
+                css: UserController.pageData.css,
+                erros: [{
+                    msg: 'O usuário já possui cadastro!'
+                }]
+            })
+        } else {
             req.body.password = bcrypt.hashSync(req.body.password, 10)
             next()
-        }  
-    }else{
+        }
+    } else {
         // res.send(UserController.pageData)
-        res.render('register', {css: UserController.pageData.css, erros: erro.errors})
+        res.render('register', {
+            css: UserController.pageData.css,
+            erros: erro.errors
+        })
     }
 }
 
